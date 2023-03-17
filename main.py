5f80f34f91839a1f931981cd90eb25a909d61c25
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-17 13:30:34 -0600

    Loss, optimizer and checkpoints

--- main.py
+++ main.py
@@ -97,4 +97,28 @@ discriminator.summary()
 decision = discriminator(generated_image)
 print(decision)
 
+# Losses
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+# Discriminator loss
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+# Generator loss
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+# Optimizers
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+
 print("Done!")
