caacc553fe87b19fe205ab4af68195e92febceae
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-17 13:29:53 -0600

    Discriminator model

--- main.py
+++ main.py
@@ -66,3 +66,35 @@ def make_generator_model():
 generator = make_generator_model()
 generator.summary()
     
+noise = tf.random.normal([1, 1000])
+generated_image = generator(noise, training=False)
+
+plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+
+    model.add(layers.Conv2D(4, (5, 5), strides=(2, 2), padding='same', input_shape=[1248, 648, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(8, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(16, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
+discriminator = make_discriminator_model()
+discriminator.summary()
+
+decision = discriminator(generated_image)
+print(decision)
+
+print("Done!")
