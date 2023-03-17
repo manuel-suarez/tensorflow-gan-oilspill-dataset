17a1df62f9fdf11a9a7a39f019972170725fbfa2
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-17 11:28:46 -0600

    Import libraries

---
+++ main.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+print(tf.__version__)
+
+import glob
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+from keras import layers
+import time
+
+from IPython import display
