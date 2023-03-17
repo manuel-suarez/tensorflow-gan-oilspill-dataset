a054cf6ec653fa31a9592b62517c7e4b60e44f84
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-17 13:31:20 -0600

    Training loop

--- main.py
+++ main.py
@@ -120,5 +120,59 @@ checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                  generator=generator,
                                  discriminator=discriminator)
 
+# Training
+EPOCHS = 50
+noise_dim = 1000
+num_examples_to_generate = 16
+
+# You will reuse this seed overtime (so it's easier)
+# to visualize progress in the animated GIF)
+seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
+# Notice the use of `tf.function`
+# This annotation causes the function to be "compiled".
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+    with tf.GradientTape() as generator_tape, tf.GradientTape() as discriminator_tape:
+        generated_images = generator(noise, training=True)
+
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = generator_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = discriminator_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+def train(dataset, epochs):
+    for epoch in range(epochs):
+        start = time.time()
+
+        for image_batch in dataset:
+            train_step(image_batch)
+
+        # Produce images for the GIF as you go
+        display.clear_output(wait=True)
+        generate_and_save_images(generator,
+                                 epoch + 1,
+                                 seed)
+
+        # Save the model every 15 epochs
+        if (epoch + 1) % 15 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
+
+        print('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+    # Generate after the final epoch
+    display.clear_output(wait=True)
+    generate_and_save_images(generator,
+                             epochs,
+                             seed)
 
 print("Done!")
