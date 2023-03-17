542e2de624e85408be01409886e6e1f8be1649c4
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-17 13:31:45 -0600

    Image generation and train

--- main.py
+++ main.py
@@ -175,4 +175,37 @@ def train(dataset, epochs):
                              epochs,
                              seed)
 
+def generate_and_save_images(model, epoch, test_input):
+    # Notice `training` is set to False.
+    # This is so all layers run in inference mode (batchnorm).
+    predictions = model(test_input, training=False)
+
+    fig = plt.figure(figsize=(4, 4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+    plt.show()
+
+train(train_dataset, EPOCHS)
+checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+# Display a single image using the epoch number
+def display_image(epoch_no):
+    return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+display_image(EPOCHS)
+
+anim_file = 'dcgan.gif'
+with imageio.get_writer(anim_file, mode='I') as writer:
+    filenames = glob.glob('image*.png')
+    filenames = sorted(filenames)
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+    image = imageio.imread(filename)
+    writer.append_data(image)
+
 print("Done!")
