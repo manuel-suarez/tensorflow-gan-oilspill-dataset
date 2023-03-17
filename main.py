6715b9e35722f84a7dccaf4756e939b774e862ae
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-17 12:11:42 -0600

    Data loading

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ from keras import layers
 import time
 
 from IPython import display
+
+BATCH_SIZE = 32
+IMG_HEIGHT = 650
+IMG_WIDTH = 1250
+
+# Data loading
+datadir = "C:\\Users\\masua\\Downloads\\Cimat\\oil-spill-dataset\\images"
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    datadir,
+    label_mode=None,
+    batch_size=BATCH_SIZE,
+    image_size=(IMG_HEIGHT, IMG_WIDTH)
+)
+
